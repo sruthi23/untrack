a08848d47db0993a00a19a8fd4e6e038bca53709
niksmac <nikhil@lightrains.com>
2019-04-04 23:03:15 +0530

    Add home page

--- app/components/CustomDomains.js
+++ app/components/CustomDomains.js
@@ -1,44 +1,34 @@
 // @flow
 import React, { Component } from 'react';
-import { Layout, Table, AutoComplete, Button } from 'element-react';
+import { Layout } from 'element-react';
 import Head from './Head';
 import LeftMenu from './LeftMenu';
 
 import db from '../utils/db';
-import download from '../utils/Downloader';
-
-const shell = require('shelljs');
-const sudo = require('sudo-prompt');
-
-const options = {
-  name: 'Untrack'
-};
 
 export default class CustomDomains extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      icon: 'assets/angry.svg',
+      text: 'Shoot! You are being spied.'
+    };
   }
 
-  buttonClick() {
-    download(
-      'https://download.damieng.com/fonts/original/EnvyCodeR-PR7.zip',
-      'app/assets/hosts/envy-code-r.zip',
-      (bytes, percent) => console.log(`Downloaded ${bytes} (${percent})`)
-    );
-
-    // const isInitial = db.get('initial').value();
-    // const script = isInitial === true ? 'init.sh' : 'reset.sh';
-    // sudo.exec(`sh app/scripts/${script}`, options, (error, stdout, stderr) => {
-    //   if (error) throw error;
-    //   console.log(`stdout: ${stdout}`);
-    //   db.set('initial', false).write();
-    // });
+  componentDidMount() {
+    const isRunning = db.get('isRunning').value();
+    if (isRunning) {
+      this.setState({
+        icon: 'assets/cool.svg',
+        text: 'Great you are saved from;'
+      });
+    }
   }
 
   render() {
     return (
       <div className="container">
-        <Layout.Row>
+        <Layout.Row className="grid">
           <Layout.Col span="6">
             <div className="grid-content home-left">
               <LeftMenu />
@@ -47,12 +37,47 @@ export default class CustomDomains extends Component {
           <Layout.Col span="18">
             <Head />
 
-            <div className="grid-content home-right">
-              <h1>Filter List</h1>
+            <div className="grid-content home-right welcome">
+              <Layout.Row gutter="20">
+                <Layout.Col span="24">
+                  <div className="grid-content masthead">
+                    <img src={this.state.icon} width="80" />
+                    <h1>{this.state.text}</h1>
+                  </div>
+                </Layout.Col>
+              </Layout.Row>
+              <div className="cards">
+                <Layout.Row gutter="20" justify="space-around" type="flex">
+                  <Layout.Col span="12">
+                    <div className="grid-content img">
+                      <img src="assets/spy.svg" width="80" className="img" />
+                      <p>1635 General trackers</p>
+                    </div>
+                  </Layout.Col>
+                  <Layout.Col span="12" offset="1">
+                    <div className="grid-content img">
+                      <img src="assets/gambling.svg" width="80" />
+                      <p>976 Gambling websites</p>
+                    </div>
+                  </Layout.Col>
+                </Layout.Row>
 
-              <Button type="primary" onClick={this.buttonClick.bind(this)}>
-                Test this
-              </Button>
+                <Layout.Row gutter="20" justify="space-around" type="flex">
+                  <Layout.Col span="12">
+                    <div className="grid-content img">
+                      <img src="assets/sex.svg" width="80" />
+                      <p>345 Porn websites</p>
+                    </div>
+                  </Layout.Col>
+
+                  <Layout.Col span="12" offset="1">
+                    <div className="grid-content img">
+                      <img src="assets/social.svg" width="80" />
+                      <p>564 Social Media</p>
+                    </div>
+                  </Layout.Col>
+                </Layout.Row>
+              </div>
             </div>
           </Layout.Col>
         </Layout.Row>
